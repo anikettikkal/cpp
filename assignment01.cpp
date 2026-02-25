47850f2fbef0ff4816736e16a4547754ce02db66
anikettikkal <tikkalaniket2019@gmail.com>
2026-02-25 20:50:37 +0530

    second question completed

--- assignment01.cpp
+++ assignment01.cpp
@@ -3,11 +3,30 @@ using namespace std;
 main()
 {
 // Write a program scan year and print year is leap or Not.
-    int year;
-    cout<<"Enter Year:";
-    cin>>year;
-    if(year%4==0)
-    cout<<"year is leap year";
-        else
-        cout<<"year is not leap year";
+    // int year;
+    // cout<<"Enter Year:";
+    // cin>>year;
+    // if(year%4==0)
+    // cout<<"year is leap year";
+    //     else
+    //     cout<<"year is not leap year";
+
+/*
+Write a program scan number between 1 to 700
+and print
+number between 1 to 400
+number between 401 to 700
+Number not in 1 to 700
+*/
+
+    int num;
+    cout<<"Enter number between 1 to 700:";
+    cin>>num;
+    if(num>=1 && num<=400)
+       cout<<"number between 1 to 400";
+       else
+         if(num>=401 && num<=700)
+            cout<<"number between 401 to 700";
+            else
+            cout<<"Number not in 1 to 700";
 }
