7b95e33f27e18a2851722c9d516699b4e7622738
anikettikkal <tikkalaniket2019@gmail.com>
2026-02-25 20:33:38 +0530

    cpp assignment first question solve

---
+++ assignment01.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include<iostream>
+main()
+{
+// Write a program scan year and print year is leap or Not.
+    int year;
+    cout<<"Enter Year:";
+    cin>>year;
+    if(year%4==0)
+    cout<<"year is leap year";
+        else
+        cout<<"year is not leap year";
+}
