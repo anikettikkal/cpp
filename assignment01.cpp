002124598970de342c0069f63dafa48a109a7d6a
anikettikkal <tikkalaniket2019@gmail.com>
2026-02-25 21:17:01 +0530

    fourth question solve

--- assignment01.cpp
+++ assignment01.cpp
@@ -37,18 +37,50 @@ main()
     print person is senior citizen
     */
 
-    int age;
-    cout<<"\nEnter Age of Person:";
-    cin>>age;
-    if(age>=0 && age<18)
-        cout<<"Person is Minor";
-        else 
-            if(age>=18 && age<60)
-                cout<<"Person is Major";
-                else
-                    if(age>=60 && age<=100)
-                    cout<<"Person is Senior Citizen";
-                    else
-                    cout<<"Person is outoff age";
-                
+    // int age;
+    // cout<<"\nEnter Age of Person:";
+    // cin>>age;
+    // if(age>=0 && age<18)
+    //     cout<<"Person is Minor";
+    //     else
+    //         if(age>=18 && age<60)
+    //             cout<<"Person is Major";
+    //             else
+    //                 if(age>=60 && age<=100)
+    //                 cout<<"Person is Senior Citizen";
+    //                 else
+    //                 cout<<"Person is outoff age";
+
+    /*
+    Write program which print bill
+    product name rate and quantity
+    calculate amount
+    calculate discount if amount >5000
+    then give  discount  10% on amount
+    other
+    give discount 5% on amount
+    calculate net amount
+    */
+
+    char product_name[20];
+    float rate, amount;
+    int quantity;
+    cout << "\nEnter Product Name:";
+    cin >> product_name;
+    cout << "\nEnter Rate of Product:";
+    cin >> rate;
+    cout << "\nEnter Quantity of Product:";
+    cin >> quantity;
+
+    amount = rate * quantity;
+    if (amount > 5000)
+    {
+        amount = amount - (amount * 10 / 100);
+        cout << "\nAmount after 10% discount: " << amount;
+    }
+    else
+    {
+        amount = amount - (amount * 5 / 100);
+        cout << "\nAmount after 5% discount: " << amount;
+    }
 }
