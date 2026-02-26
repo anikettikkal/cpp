2240712537e7ac1d1b0f1003509ab5903215ee4d
anikettikkal <tikkalaniket2019@gmail.com>
2026-02-26 17:55:14 +0530

    second assignment start and first question solve

---
+++ assignment02.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+#include<iostream>
+main()
+{
+    //1. write prog which create menu +,-,*,/
+    /*
+    char ch,choice;
+    cout<<"\nChoice Operator +,-,*,/";
+    cout<<"\nEnter operator";
+    cin>>ch;
+    switch(ch)
+    {
+        case '+':
+        cout<<"Addition";
+        break;
+        case '-':
+        cout<<"Substraction";
+        break;
+        case '*':
+        cout<<"Multiplication";
+        break;
+        case '/':
+        cout<<"Division";
+        break;
+        default:
+        cout<<"Invalid Operator";
+    }
+    cout<<"\nEnd main Programe";
+    */
+
+    
+}
