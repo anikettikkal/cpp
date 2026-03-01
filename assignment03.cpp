1d77f59ef6b034bc08481cac18446790d03d1813
anikettikkal <tikkalaniket2019@gmail.com>
2026-03-01 21:17:08 +0530

    assignment on for loop and nested for loop

---
+++ assignment03.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include<iostream>
+
+int main(){
+
+    /*
+    write a prog which print pattern
+    1
+    1 2 
+    1 2 3
+    1 2 3 4
+    1 2 3
+    1 2
+    1
+    */
+   
+    int i,j,n;
+    cout<< "\nEnter the Value of n";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<<j<<" * ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
