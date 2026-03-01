52968a5b78f9dde0ac97e5dbc8b989677a8f7813
anikettikkal <tikkalaniket2019@gmail.com>
2026-03-01 22:09:43 +0530

    nested for loop programe end

--- assignment03.cpp
+++ assignment03.cpp
@@ -1,36 +1,82 @@
 using namespace std;
-#include<iostream>
+#include <iostream>
 
-int main(){
+int main()
+{
 
     /*
     1.write a prog which print pattern
     1
-    1 2 
+    1 2
     1 2 3
     1 2 3 4
     1 2 3
     1 2
     1
     */
-   
-    int i,j,n;
-    cout<< "\nEnter the Value of n";
-    cin>>n;
-    for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-            cout<< j<<" ";
-        }
-        cout<<"\n";
-    }
 
-    for(i=n-1; i>=1;i--){
-        for(j=1;j<=i;j++){
-            cout<< j<<" ";
-        }
-        cout<<"\n";
-    }
+    // int i,j,n;
+    // cout<< "\nEnter the Value of n";
+    // cin>>n;
+    // for(i=1;i<=n;i++){
+    //     for(j=1;j<=i;j++){
+    //         cout<< j<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
 
-    
+    // for(i=n-1; i>=1;i--){
+    //     for(j=1;j<=i;j++){
+    //         cout<< j<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    /*
+    2.write a prog which print pattern
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    */
+
+    // int i, j, n;
+    // int num = 1;
+
+    // cout << "Enter value of n: ";
+    // cin >> n;
+
+    // for (i = 1; i <= n; i++)
+    // {
+    //     for (j = 1; j <= i; j++)
+    //     {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    3.write a prgm which print
+    A
+    B C
+    D E F
+    G H I J
+    */
+
+    // int i,j,n;
+    // char ch =65;
+    // cout<< "Enter the value of n: ";
+    // cin>>n;
+    // for(i=1;i<=n;i++){
+    //     for(j=1;j<=i;j++){
+    //         cout<< ch<<" ";
+    //         ch++;
+    //     }
+    //     cout<<"\n";
+    // }
+
+    //============================= array prgm start==================
+
     return 0;
 }
