afcd905c0bee789f576dcbd6153b39e92a6f60a2
anikettikkal <tikkalaniket2019@gmail.com>
2026-03-02 21:06:44 +0530

    second question completed

---
+++ assignment04/question2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main(){
+    int arr[5],i;
+    for(i=0;i<5;i++){
+        cout<<"Enter a number: ";
+        cin>>arr[i];
+    }
+
+    int num,count=0;
+    cout<<"Enter a number to search: ";
+    cin>>num;
+
+    for(i=0;i<5;i++){
+        if(num==arr[i]){
+            count++;
+        }
+        cout<<arr[i]<<endl;
+    }
+
+    cout<<"Number of times "<<num<<" is found in the array: "<<count<<endl;
+}
